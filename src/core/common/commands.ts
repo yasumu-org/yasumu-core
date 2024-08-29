e18441ef71f1fdc53c5443cdddfb4452bb77cb9a
twlite <46562212+twlite@users.noreply.github.com>
2024-08-29 21:32:18 +0545

    chore: release 0.0.9

--- src/core/common/commands.ts
+++ src/core/common/commands.ts
@@ -6,7 +6,7 @@ export const Commands = {
   GetCurrentWorkspace: 'get_current_workspace',
   SetCurrentWorkspace: 'set_current_workspace',
   ClearCurrentWorkspaceSession: 'clear_current_workspace_session',
-  // Http
+  // Network
   GetLocalAddress: 'get_local_address',
   // Smtp
   StartSmtpServer: 'start_smtp_server',
@@ -14,6 +14,16 @@ export const Commands = {
   GetEmails: 'get_emails',
   ClearEmails: 'clear_emails',
   IsSmtpServerRunning: 'is_smtp_server_running',
+  GetEmail: 'get_email',
+  MarkAsUnread: 'mark_as_unread',
+  MarkAllAsRead: 'mark_all_as_read',
+  MarkAllAsUnread: 'mark_all_as_unread',
+  GetUnreadEmailsCount: 'get_unread_emails_count',
+  GetReadEmailsCount: 'get_read_emails_count',
+  GetAllEmailsCount: 'get_all_emails_count',
+  DeleteEmail: 'delete_email',
+  // Scripting
+  EvaluateJavaScript: 'evaluate_javascript',
 } as const;
 
 export type Commands = (typeof Commands)[keyof typeof Commands];
@@ -31,16 +41,29 @@ export interface StartSmtpServerCommand {
 }
 
 export interface CommandsInvocationMap {
+  // Workspace
   [Commands.GetCurrentWorkspace]: CommandInvocation<{}, string | null>;
   [Commands.SetCurrentWorkspace]: CommandInvocation<
     SetCurrentWorkspaceCommand,
     void
   >;
   [Commands.ClearCurrentWorkspaceSession]: CommandInvocation<{}, void>;
+  // Network
   [Commands.GetLocalAddress]: CommandInvocation<{}, string>;
+  // Smtp
   [Commands.StartSmtpServer]: CommandInvocation<StartSmtpServerCommand, void>;
   [Commands.StopSmtpServer]: CommandInvocation<{}, void>;
   [Commands.GetEmails]: CommandInvocation<{}, YasumuMail[]>;
   [Commands.ClearEmails]: CommandInvocation<{}, void>;
   [Commands.IsSmtpServerRunning]: CommandInvocation<{}, boolean>;
+  [Commands.GetEmail]: CommandInvocation<{ id: string }, YasumuMail | null>;
+  [Commands.MarkAsUnread]: CommandInvocation<{ id: string }, void>;
+  [Commands.MarkAllAsRead]: CommandInvocation<{}, void>;
+  [Commands.MarkAllAsUnread]: CommandInvocation<{}, void>;
+  [Commands.GetUnreadEmailsCount]: CommandInvocation<{}, number>;
+  [Commands.GetReadEmailsCount]: CommandInvocation<{}, number>;
+  [Commands.GetAllEmailsCount]: CommandInvocation<{}, number>;
+  [Commands.DeleteEmail]: CommandInvocation<{ id: string }, void>;
+  // Scripting
+  [Commands.EvaluateJavaScript]: CommandInvocation<{ code: string }, any>;
 }
